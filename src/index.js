c4dda3b4db9b037a9aa27ac1a9cccdf504868ac8
John <jaricaya@gmail.com>
2024-05-18 16:39:56 -0700

    advanced deliverables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const handleClick = (ramen) => {
   const ramenDetail = document.getElementById('ramen-detail')
   const ratingDisplay = document.getElementById('rating-display')
   const commentDisplay = document.getElementById('comment-display')
+  const editRamenForm = document.getElementById("edit-ramen")
 
   ramenDetail.querySelector('img').src = ramen.image
   ramenDetail.querySelector('img').alt = ramen.name
@@ -11,6 +12,17 @@ const handleClick = (ramen) => {
  
   ratingDisplay.textContent = ramen.rating
   commentDisplay.textContent = ramen.comment 
+
+  if(editRamenForm) {
+   const editRating = editRamenForm.querySelector("#edit-ramen")
+   const editComment = editRamenForm.querySelector("#edit-comment")
+  
+  if(editRating && editComment) {
+    editRating.value = ramen.rating;
+    editComment.value = ramen.comment;
+  } 
+  editRamenForm.dataset.currentRamenId = ramen.id;
+  }  
 };
 
 const addSubmitListener = () => {
@@ -18,6 +30,7 @@ const addSubmitListener = () => {
   const ramenDataMenu = document.getElementById('ramen-menu')
   newRamenDataForm.addEventListener('submit', function(event){
     event.preventDefault()
+
     let ramen = {
       'image': newRamenDataForm.querySelector('#new-image').value,
       'name': newRamenDataForm.querySelector('#new-name').value,
@@ -25,6 +38,18 @@ const addSubmitListener = () => {
       'rating' : newRamenDataForm.querySelector('#new-rating').value,
       'comment': newRamenDataForm.querySelector('#new-comment').value
     }
+    fetch('http://localhost:3000/ramens', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify(ramen)
+    })
+    .then(response => response.json())
+    .then(newRamen => {
+      ramen.id = newRamen.id;
+      ramenData.push(ramen);
+    })
     const div = document.createElement('div')
     const image = document.createElement('img')
     image.src = ramen.image
@@ -32,11 +57,82 @@ const addSubmitListener = () => {
     const name = document.createElement('h2')
     name.innerText = ramen.name
     const restaurant = document.createElement('h3')
-    restaurant.innerText = ramen.restaurant
-    div.append(image, name, restaurant)
+    restaurant.innerText = ramen.restaurant;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => handleDelete(ramen, div));
+    div.append(image, name, restaurant, deleteButton)
+   
     ramenDataMenu.append(div)
+
+    // ramen.id = ramenData +1;
+    // ramenData.push(ramen)
+  })
+  const editRamenForm = document.getElementById('edit-ramen')
+    editRamenForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      console.log("edit this form")
+      const currentRamenId = editRamenForm.dataset.currentRamenId
+      if (!currentRamenId) {
+        return
+      }
+          const updatedRating = editRamenForm.querySelector('#edit-rating').value;
+          const updatedComment = editRamenForm.querySelector('#edit-comment').value;
+
+    fetch(`http://localhost:3000/ramens/${currentRamenId}`, {
+      method: 'PATCH',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        rating: updatedRating,
+        comment: updatedComment
+      })
+    })
+    .then(response => response.json())
+    .then(updateRamen => {
+      const ratingDisplay = document.getElementById('rating-display');
+      const commentDisplay = document.getElementById('comment-display')
+
+      ratingDisplay.textContent = updatedRating
+      commentDisplay.textContent = updatedComment
+
+      const ramen = ramenData.find(ramen => ramen.id === parseInt(currentRamenId))
+      if (ramen) {
+        ramen.rating = updatedRating
+        ramen.comment = updatedComment
+    }
+    });
+  });
+};
+const handleDelete = (ramen, ramenElement) => {
+  const ramenMenu = document.getElementById("ramen-menu");
+  const ramenDetail = document.getElementById("ramen-detail");
+
+  fetch('`http://localhost:3000/ramens/${ramen.id}`', {
+    method: 'Delete'
+  })
+  .then(() => {   
+    ramenMenu.removeChild(ramenElement);
+
+    ramenData = ramenData.filter(item => item.id !== ramen.id);
+    
+    const currentRamenId = ramenDetail.querySelector('img').alt === ramen.name ? ramen.id : null;
+    if (currentRamenId) {
+      ramenDetail.querySelector("img").src = " ";
+      ramenDetail.querySelector("img").alt = " ";
+      ramenDetail.querySelector("h2").textContent = " ";
+      ramenDetail.querySelector("h3").textContent = " ";
+      document.getElementById("rating-display").textContent = " ";
+      document.getElementById("comment-display").textContent = " ";
+  
+    } 
   })
 }
+
+
 const displayRamens = () => {
   const ramenMenu = document.getElementById('ramen-menu')
   fetch('http://localhost:3000/ramens')
@@ -44,14 +140,32 @@ const displayRamens = () => {
     return response.json()
   })
   .then(function(data){
+   ramenData = data;
+    if (data.length > 0) {
+      handleClick(data[0])
+    }
     data.forEach(function(ramen){
+      const div = document.createElement("div");
       const image = document.createElement('img')
       image.src = ramen.image
       ramenMenu.append(image)
       image.addEventListener('click', () => handleClick(ramen))
+      const name = document.createElement("h2");
+      name.innerText = ramen.name;
+      const restaurant = document.createElement('h3');
+      restaurant.innerText = ramen.restaurant
+
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => handleDelete(ramen, div));
+      div.append(image, name, restaurant, deleteButton);
+      
+      ramenMenu.append(div);
     })
   })
 };
+let ramenData = [];
+
 const main = () => {
   displayRamens()
   addSubmitListener()
@@ -61,5 +175,6 @@ export {
   displayRamens,
   addSubmitListener,
   handleClick,
+  handleDelete,
   main,
 };
